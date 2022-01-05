47dba52cfd3823daadf0a9703b204532efef2156
GuillaumeHouzard <97163670+GuillaumeHouzard@users.noreply.github.com>
2022-01-05 14:17:57 +0100

    Add files via upload

---
+++ VueCV/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+.accroche {
+  margin-left: 25%;
+  margin-top: 7%;
+  position: absolute;
+}
+
+h1 {
+  color: black;
+  font-size: 40px;
+  text-transform: uppercase;
+}
+
+span {
+  color: #BD5D38;
+}
+
+a {
+  color: #BD5D38;
+  text-decoration: underline;
+}
+
+.description {
+  margin-top: 7%;
+}
+
+</style>
